1fadfc50ed77aaef4bfbbc77efaf57a5dc8cf282
Zartab Nakhwa <nzartab@gmail.com>
2021-07-06 12:57:11 +0530

    Recipe List and Recipe Item Separation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,10 @@ export class RecipeListComponent implements OnInit {
   recipes:Recipe[]=[
     new Recipe('A Healthy Salad',
     'Salad made up of veggies and spices',
-    'https://www.ndtv.com/cooks/images/tossed-mixed-salad-620.jpg')
+    'https://www.ndtv.com/cooks/images/tossed-mixed-salad-620.jpg'),
+    new Recipe('Cheezy Pizza',
+    'Its pizza, what do you want to know more',
+    'https://curlytales.com/wp-content/uploads/2019/09/pizza-e1569475959190-1280x720.jpg')
   ]
 
   constructor() { }
